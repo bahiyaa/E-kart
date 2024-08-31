bd565335c80ed4f3b34c6950c3b2e6482695c327
bahiyaa <biyya017@gmail.com>
2024-08-31 16:09:34 +0530

    design changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,13 @@ function Footer() {
     
     <>
     <div style={{width:'100%',height:"300px"}}
-    className='d-flex justify-contentcenter align-items-center flex-column mt-5 bg-primary'>
-      <div className='d-flex justify-content-evenly align-items-center mb-5 w-100' style={{color:'white'}}>
-            <div style={{width:'400px',color:'wheat'}}>
+    className='d-flex justify-contentcenter align-items-center flex-column bg-primary'>
+      <div className='d-flex justify-content-evenly align-items-center mb-5 w-100 mt-2' style={{color:'white'}}>
+            <div style={{width:'400px',color:'white'}} className='mt-4'>
                 <h4>
                 <i style={{color:'white'}} class="fa-solid fa-cart-shopping fa-bounce me-3"></i>
                     E-KART</h4>
-                    <h6>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius voluptas asperiores quis eligendi autem earum labore quibusdam voluptatem repellat amet illum dolor, est velit minima. Consequatur nostrum perferendis atque possimus.</h6>
+                    <h6 className='lh-base'>EKART is India’s largest logistics and supply chain company delivering 10 million shipments a month. Established in 2009, EKART as Flipkart’s in-house supply chain arm has powered the growth of Flipkart with innovations such as Cash on Delivery, In-a-day guarantee (50 cities), Same-day guarantee (13 cities).</h6>
             </div>
 
             <div className='d-flex flex-column'>
@@ -24,21 +24,21 @@ function Footer() {
 
             <div className='d-flex flex-column'>
               <h4>Guides</h4>
-              <Link to={'https://react-bootstrap.netlify.app/'} style={{textDecoration:'none',color:'white'}}>React</Link>
-              <Link to={'https://react-bootstrap.netlify.app/'} target='blank' style={{textDecoration:'none',color:'white'}}>React Bootstrap</Link>
-              <Link to={'https://react-bootstrap.netlify.app/'} style={{textDecoration:'none',color:'white'}}>Boots Watch</Link>
+              <Link to={'https://react-bootstrap.netlify.app/'} style={{textDecoration:'none',color:'white'}}>Reviews</Link>
+              <Link to={'https://react-bootstrap.netlify.app/'} target='blank' style={{textDecoration:'none',color:'white'}}>Help center</Link>
+              <Link to={'https://react-bootstrap.netlify.app/'} style={{textDecoration:'none',color:'white'}}>Tracking</Link>
             </div>
             <div>
               <h4>Contact</h4>
               <div className='d-flex'>
                 <input type="text" className='form-control' placeholder='Enter Email id' />
-                <button className='btn btn-warning ms-2'>Subscribe</button>
+                <button className='btn btn-secondary ms-2'>Subscribe</button>
               </div>
               <div className='d-flex justify-content-evenly mt-3'>      
-              <Link className='text-warning' style={{textDecoration:'none',color:'white'}}><i class="fa-brands fa-instagram fa-2x"></i></Link>
-              <Link className='text-warning' style={{textDecoration:'none',color:'white'}}><i class="fa-brands fa-reddit fa-2x"></i></Link>
-              <Link  className='text-warning'style={{textDecoration:'none',color:'white'}}><i class="fa-brands fa-facebook fa-2x"></i></Link>
-              <Link  className='text-warning'style={{textDecoration:'none',color:'white'}}><i class="fa-brands fa-twitter fa-2x"></i></Link>          
+              <Link  style={{textDecoration:'none',color:'white'}}><i class="fa-brands fa-instagram fa-2x"></i></Link>
+              <Link  style={{textDecoration:'none',color:'white'}}><i class="fa-brands fa-reddit fa-2x"></i></Link>
+              <Link  style={{textDecoration:'none',color:'white'}}><i class="fa-brands fa-facebook fa-2x"></i></Link>
+              <Link style={{textDecoration:'none',color:'white'}}><i class="fa-brands fa-twitter fa-2x"></i></Link>          
               </div>
             </div>
         </div>
